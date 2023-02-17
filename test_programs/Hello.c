cbc8df04032d700b5f498d85f5e99a123e2a7e7e
Salifu Sadick <sadick.salifu@gmail.com>
2023-02-17 17:01:08 +0000

    My local files

---
+++ test_programs/Hello.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/*
+ * This is just a test program that I am practicing
+ *
+ * the main function i dont know what is does
+ *
+ * Return: 0
+*/
+{
+	int main(void)
+	printf("to become software engineer by the end of 2023, Insha Allah.\n");
+	return (0);
+}
